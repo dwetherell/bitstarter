b3ae23efda17c04cf03c4aa44753766f0254d999
David Wetherell <davidwetherell@limeflies.com>
2013-07-07 05:05:37 +0000

    THis is the web.js

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs.appendFile('index.html', 'Hello World From FS Index.html', function (err) {
+  if (err) throw err;
+  console.log('The "data to append" was appended to file!');
+});
+
+
+
 app.get('/', function(request, response) {
-  response.send('index.html');
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
