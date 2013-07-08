a254445ed93b892a725e4352ea03e424f33250e5
David Wetherell <davidwetherell@limeflies.com>
2013-07-08 04:35:56 +0000

    this

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 
+app.get('/', function(request, response) {
+var fs = require("fs");
 var buffer = new Buffer("Hello World from index.html", 25);
 var data = fs.readFileSync("index.html", "utf-8");
 response.send(data);
