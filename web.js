5c35ef89785870e1890c3dc7c2e8cdeb666e2fd8
David Wetherell <davidwetherell@limeflies.com>
2013-07-08 03:59:46 +0000

    updated version1

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require("fs");
-var buffer = new buffer();
-  
-response.send(buffer.tostring("utf-8", fs.readFileSync("index.html")));
+var buffer = new Buffer("Hello World from index.html", 25);
+var data = fs.readFileSync("index.html"), buffer.toString("utf-8"));
+response.send(data);
 });
 
 var port = process.env.PORT || 5000;
