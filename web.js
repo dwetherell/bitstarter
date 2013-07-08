1725ef46d3d9777cee66940f4d3016dc543db169
David Wetherell <davidwetherell@limeflies.com>
2013-07-08 04:30:10 +0000

    this

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-app.get('/index.html', function(req, res){
-  var body = 'Hello World from the Index page!';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-});
+
+var buffer = new Buffer("Hello World from index.html", 25);
+var data = fs.readFileSync("index.html", "utf-8");
+response.send(data);
+  });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
