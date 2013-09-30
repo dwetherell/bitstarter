b8a3eea2864c4d6ab8e165b3f991a0c150125dee
David <davidwetherell@limeflies.com>
2013-09-30 11:06:30 -0400

    yes
    yes

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Hello
 =======
 bitstarter
 ==========
-What the!!
+What the!!   Ndasdasd
 This is bitstarter
 >>>>>>> 2387ad884b138c24ba8ea82bd75b5f3dabf250dc
--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require("fs");
-var buffer = new Buffer("Hello World from index.html", 25);
+var buffer = new Buffer("Hello World from Yes index.html", 25);
 var data = fs.readFileSync("index.html", "utf-8");
 response.send(data);
   });
